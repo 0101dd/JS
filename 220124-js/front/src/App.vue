aa0a3255aeff6839da2bedc5531309c157340b54
0101dd <aa09335665@gmail.com>
2022-01-24 16:11:55 +0800

    chat

---
+++ 220124-js/front/src/App.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+v-app
+  v-app-bar(app color='primary' dark)
+    v-toolbar-title 聊天室
+    v-spacer
+    v-btn(text to='/login' v-if='!user.isLogin') 登入
+    v-btn(text to='/register' v-if='!user.isLogin') 註冊
+    span(v-else) {{ user.account }}
+  v-main
+    router-view
+</template>
